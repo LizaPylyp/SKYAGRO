5230ea6014746f21121d3e469d080f50cb464a4a
Liza <lizkapylypchuk123456@gmail.com>
2023-04-30 18:46:26 +0300

    Update sliker.js

--- js/sliker.js
+++ js/sliker.js
@@ -1,18 +1,18 @@
 let offset = 0;
-const sliderLine = document.querySelector('.video-works');
+const slikerLine = document.querySelector('.video-works');
 
 document.querySelector('.video-nextt').addEventListener('click', function(){
-offset = offset + 353;
-if (offset >1059) {
+offset=offset +353;
+if (offset >1412) {
   offset = 0;
 }
-sliderLine.style.left = -offset + 'px';
+slikerLine.style.left = -offset + 'px';
 });
 
-document.querySelector('.video-prew').addEventListener('click', function(){
-  offset = offset - 353;
-  if (offset <0) {
-    offset = 1059;
+document.querySelector('.video-prew').addEventListener('click',function(){
+  offset= offset - 353;
+  if (offset <0){
+    offset= 1412;
   }
-  sliderLine.style.left = -offset + 'px';
-  });
+  slikerLine.style.left = -offset + 'px';
+});
