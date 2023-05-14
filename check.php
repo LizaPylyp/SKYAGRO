dde7a35d950ead6ee08197183de3aca59e190e93
Liza <lizkapylypchuk123456@gmail.com>
2023-05-14 17:35:41 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,39 +1,21 @@
 <?php
-$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
-$location = filter_var(trim($_POST['location']), FILTER_SANITIZE_STRING);
-$volume = filter_var(trim($_POST['volume']), FILTER_SANITIZE_STRING);
-$telefon = filter_var(trim($_POST['telefon']), FILTER_SANITIZE_STRING);
 
-if(mb_strlen($location) < 10 || mb_strlen($location) > 400){
-    echo "Недопустима довжина адреси";
-    exit();
-} else if(mb_strlen($telefon) < 7 || mb_strlen($telefon) > 19){
-    echo "Недопустима довжина номеру телефону";
-    exit();
+$connect = mysqli_connect(
+    host: 'localhost',
+    user: 'root@localhost',
+    password: '',
+    database: 'SkyAgro'
+);
+
+if (!$connect) {
+    die ('Error connect to database!');
 }
 
-$mysqli = new mysqli('localhost', 'root', 'password', 'database_name');
-if ($mysqli->connect_errno) {
-    echo "Помилка з'єднання з базою даних: " . $mysqli->connect_error;
-    exit();
-}
+$name = $_POST['name'];
+$location = $_POST['location'];
+$volume = $_POST['volume'];
+$telefon = $_POST['telefon'];
 
-$sql = "INSERT INTO regi (name, location, volume, telefon) VALUES (?, ?, ?, ?)";
-$stmt = $mysqli->prepare($sql);
-if (!$stmt) {
-    echo "Помилка підготовки запиту: " . $mysqli->error;
-    exit();
-}
+mysqli_query($connect, "INSERT INTO `forma` (`name`, `location`, `volume`, `telefon`) VALUES ('$name', '$location', '$volume', '$telefon')");
 
-$stmt->bind_param("ssss", $name, $location, $volume, $telefon);
-if (!$stmt->execute()) {
-    echo "Помилка виконання запиту: " . $stmt->error;
-    exit();
-}
-
-$stmt->close();
-$mysqli->close();
-
-header('Location: /');
-exit();
 ?>
