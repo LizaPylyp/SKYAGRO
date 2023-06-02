fbca62ad5efdbf03a6cfe80ee5d7b9d0607ba601
Liza <lizkapylypchuk123456@gmail.com>
2023-06-02 21:38:41 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,21 +1,14 @@
 <?php
-
-$connect = mysqli_connect(
-    host: 'localhost',
-    user: 'root@localhost',
-    password: '',
-    database: 'SkyAgro'
-);
-
-if (!$connect) {
-    die ('Error connect to database!');
-}
-
-$name = $_POST['name'];
-$location = $_POST['location'];
-$volume = $_POST['volume'];
-$telefon = $_POST['telefon'];
-
-mysqli_query($connect, "INSERT INTO `forma` (`name`, `location`, `volume`, `telefon`) VALUES ('$name', '$location', '$volume', '$telefon')");
-
+$name = filter_var(trim($_POST['name']),
+FILTER_SANITIZE_STRING);
+$location= filter_var(trim($_POST['location']),
+FILTER_SANITIZE_STRING);
+$volume= filter_var(trim($_POST['volume']),
+FILTER_SANITIZE_STRING);
+$telefon= filter_var(trim($_POST['telefon']),
+FILTER_SANITIZE_STRING);
+$mysql = new mysqli('localhost', 'root', 'root', 'skyagro8 skymodal');
+$mysgl->query('INSERT INTO `Forma`( name, location, volume, telefon)
+VALUES($name, $location, $volume, $telefon)');
+$mysql-> close();
 ?>
