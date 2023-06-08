3b651b8df333b14350053fcaa4d8aaca9501208e
Liza <lizkapylypchuk123456@gmail.com>
2023-06-08 21:39:48 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,24 +1,32 @@
 <?php
-$name = filter_var(trim($_POST['name']),
-FILTER_SANITIZE_STRING);
-$location= filter_var(trim($_POST['location']),
-FILTER_SANITIZE_STRING);
-$volume= filter_var(trim($_POST['volume']),
-FILTER_SANITIZE_STRING);
-$telefon= filter_var(trim($_POST['telefon']),
-FILTER_SANITIZE_STRING);
-$mysql = new mysqli('localhost', 'root', 'sky17-sky3108', 'skyagro8 skymodal');
+$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+$location = filter_var(trim($_POST['location']), FILTER_SANITIZE_STRING);
+$volume = filter_var(trim($_POST['volume']), FILTER_SANITIZE_STRING);
+$telefon = filter_var(trim($_POST['telefon']), FILTER_SANITIZE_STRING);
+
+$mysqli = new mysqli('localhost', 'skyagro8_skymodal', 'sky17-sky3108', 'skyagro8_skymodal');
+
 if ($mysqli->connect_errno) {
     echo "Помилка підключення до MySQL: " . $mysqli->connect_error;
     exit();
 }
 
-$query = "INSERT INTO `Forma` (name, location, volume, telefon) VALUES ('$name', '$location', '$volume', '$telefon')";
+$query = "INSERT INTO Forma (name, location, volume, telefon) VALUES (?, ?, ?, ?)";
+$stmt = $mysqli->prepare($query);
 
-if ($mysqli->query($query) === TRUE) {
+if (!$stmt) {
+    echo "Помилка при підготовці запиту: " . $mysqli->error;
+    exit();
+}
+
+$stmt->bind_param("ssss", $name, $location, $volume, $telefon);
+
+if ($stmt->execute()) {
     echo "Запис успішно додано до бази даних";
 } else {
-    echo "Помилка при виконанні запиту: " . $mysqli->error;
+    echo "Помилка при виконанні запиту: " . $stmt->error;
 }
-$mysql-> close();
+
+$stmt->close();
+$mysqli->close();
 ?>
